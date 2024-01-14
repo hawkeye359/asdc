92dcb286cb3798ccb81dbc0083163f7ddf2254a5
Mohd Huzaifa <mohdhuzaifa359@gmail.com>
2024-01-14 10:39:39 +0530

    something

--- next.config.js
+++ next.config.js
@@ -9,10 +9,10 @@ const nextConfig = {
           {
             key: "Access-Control-Allow-Origin",
             value: "*",
-          }, // replace this your actual origin
+          },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+            value: "OPTIONS,GET,DELETE,PATCH,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
